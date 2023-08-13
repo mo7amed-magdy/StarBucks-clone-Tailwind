32252be40c398bc7e52cf9008cfa77ee3e58ad77
Mohamed Magdy <mo7amed.magdy28@gmail.com>
2023-08-13 14:16:19 +0300

    final copy

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/*/.{html,js}',
+    './pages/*/.{html,js}',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: ["14px"],
+      },
+      colors: {
+        starGreen: "#D4E9E2",
+        darkGreen: "#1E3932",
+        lightText: "rgba(0,0,0, .58)",
+        titleText: "rgba(0,0,0, .87)",
+      },
+    },
+  },
+  plugins: [],
+}
+
